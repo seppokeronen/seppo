743e05131fc56d0046359b86bdd68426a7ca3d54
Seppo Keronen <seppo.keronen@gmail.com>
2023-11-26 13:16:13 +0200

    V 0.11

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
 
     let oriented = true,
         alpha_base = 0.0, beta_base = 0.0, gamma_base = 0.0,
-        alpha = 0.0, beta = 0.0, gamma = 0.0;
+        alpha = 0.0, beta = 0.0, gamma = 0.0,
 
-    let alpha_span = document.getElementById('alpha_span'),
+        screen_width = window.innerWidth,
+        screen_height = window.innerHeight,
+        muX = 6.2 * screen_width / 360.0,
+        muY = 6.2 * screen_height / 360.0,
+
+        alpha_span = document.getElementById('alpha_span'),
         beta_span  = document.getElementById('beta_span'),
         gamma_span = document.getElementById('gamma_span');
         alpha_meter = document.getElementById('alpha_meter'),
         beta_meter  = document.getElementById('beta_meter'),
-        gamma_meter = document.getElementById('gamma_meter');
+        gamma_meter = document.getElementById('gamma_meter'),
+
+        tiles = document.getElementsByClassName("tile");
 
         function rawOrient(event) {
             alpha = event.alpha;
@@ -33,6 +40,13 @@
             alpha = event.alpha - alpha_base;
             beta = event.beta - beta_base;
             gamma = event.gamma - gamma_base;
+            dx = 0;
+            dy = muY * beta;
+            // div.style.transform = "translate(5px,10px)";
+            // div.style.translate = '0px 50px';
+            for (const tile of tiles) {
+                tile.style.translate = `${dx}px ${dy}px`;
+            }
         }
     
     let permit = (listener) => {
@@ -46,21 +60,18 @@
     }
 
     let initSensors = () => {
-        init.style.display = 'none';
+        document.getElementById('init').style.display = 'none';
         document.getElementById('info').style.display = 'block';
         permit(rawOrient);
     }
     
     let initContent = () => {
-        init.style.display = 'none';
+        document.getElementById('init').style.display = 'none';
         document.getElementById('content').style.display = 'block';
         permit(deltaOrient);
     }
-    
-    let init = document.getElementById('init'),
-        sbtn = document.getElementById('sensor_button'),
-        cbtn = document.getElementById('content_button');
-    sbtn.addEventListener('click', initSensors);
-    cbtn.addEventListener('click', initContent);
+
+    document.getElementById('sensor_button').addEventListener('click', initSensors);
+    document.getElementById('content_button').addEventListener('click', initContent);
 
 })()
