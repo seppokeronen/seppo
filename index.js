7c7b97309cd604efb1beea09361ed0c33d209e37
Seppo Keronen <seppo.keronen@gmail.com>
2023-11-26 15:46:46 +0200

    V 0.15

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
         screen_width = window.innerWidth,
         screen_height = window.innerHeight,
-        muX = 6.2 * screen_width / 360.0,
+        muX = 12.4 * screen_width / 360.0,
         muY = 6.2 * screen_height / 360.0,
 
         alpha_span = document.getElementById('alpha_span'),
@@ -18,36 +18,36 @@
 
         tiles = document.getElementsByClassName("tile");
 
-        function rawOrient(event) {
-            alpha = event.alpha;
-            beta = event.beta;
-            gamma = event.gamma;
-            alpha_span.innerHTML = alpha
-            beta_span.innerHTML = beta
-            gamma_span.innerHTML = gamma
-            alpha_meter.value = alpha
-            beta_meter.value = beta
-            gamma_meter.value = gamma
-        }
+    function rawOrient(event) {
+        alpha = event.alpha;
+        beta = event.beta;
+        gamma = event.gamma;
+        alpha_span.innerHTML = alpha
+        beta_span.innerHTML = beta
+        gamma_span.innerHTML = gamma
+        alpha_meter.value = alpha
+        beta_meter.value = beta
+        gamma_meter.value = gamma
+    }
     
-        function deltaOrient(event) {
-            if (oriented === false) {
-                alpha_base = event.alpha;
-                beta_base  = event.beta;
-                gamma_base = event.gamma;
-                oriented = true;
-            }
-            alpha = event.alpha - alpha_base;
-            beta = event.beta - beta_base;
-            gamma = event.gamma - gamma_base;
-            dx = 0;
-            dy = muY * beta;
-            // div.style.transform = "translate(5px,10px)";
-            // div.style.translate = '0px 50px';
-            for (const tile of tiles) {
-                tile.style.translate = `${dx}px ${dy}px`;
-            }
+    function deltaOrient(event) {
+        if (oriented === false) {
+            alpha_base = event.alpha;
+            beta_base  = event.beta;
+            gamma_base = event.gamma;
+            oriented = true;
         }
+        alpha = event.alpha - alpha_base;
+        beta = event.beta - beta_base;
+        gamma = event.gamma - gamma_base;
+        dx = muX * gamma;
+        dy = muY * beta;
+        // div.style.transform = "translate(5px,10px)";
+        // div.style.translate = '0px 50px';
+        for (const tile of tiles) {
+            tile.style.translate = `${dx}px ${dy}px`;
+        }
+    }
     
     let permit = (listener) => {
         DeviceOrientationEvent.requestPermission()
